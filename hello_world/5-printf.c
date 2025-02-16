c340820e88ed9f2b4b3a507d13e91c7331ef6a93
OsmanBagci3 <osman.bagci3@gmail.com>
2025-02-16 21:47:28 +0100

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art,
+ * main - Prints "with proper grammar, but the outcome is
+ * 		  a piece of art,
  *
  * Return : always 0.
  */
