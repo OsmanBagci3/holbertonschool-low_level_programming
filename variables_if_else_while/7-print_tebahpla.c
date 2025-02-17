4528e744da0c8009876455c9788cd4f4f45b5b52
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 14:51:36 +0100

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+/*
+ * File: 7-print_tebahpla.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints reversed alphabet
+ *
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch ='a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
