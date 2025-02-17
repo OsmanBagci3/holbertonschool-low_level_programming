3885fea88a52f6161ca5f1bfe56819885ae24f27
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 14:46:14 +0100

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+/*
+ * File: 6-print_numberz.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints digit numberz
+ *
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
