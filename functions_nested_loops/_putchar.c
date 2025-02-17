b13e4ee717f91614dad0cfd4e87083da03bc72eb
OsmanBagci3 <osman.bagci3@gmail.com>
2025-02-17 21:32:39 +0100

    0

---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+/*
+ * File: 9-print_comb.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
