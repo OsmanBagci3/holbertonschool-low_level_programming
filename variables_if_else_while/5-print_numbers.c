957a3b6e11b4b9c672d64a342dd94318de961a38
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 14:41:05 +0100

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * File: 5-print_numbers.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints digit numbers from 0 to 10
+ *
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d\n", i);
+	}
+}
