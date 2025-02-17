1d47c931818c769a9739fdadec0f7d2d4de3f5af
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 15:48:54 +0100

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+/*
+ * File: 9-print_comb.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints combinaison of single single-gidit number
+ *
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+
+	return (0);
+}
