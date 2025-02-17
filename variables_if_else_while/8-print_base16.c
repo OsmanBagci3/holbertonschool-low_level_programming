1bfb0d4ad512ae801b5b65e45de124ea8f1a0f81
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 15:04:53 +0100

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+/*
+ * File: 7-print_base16.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet all numbers 
+ *	of base 16 in lowercase
+ *
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 15; n++)
+	{
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar('a' + (n - 10));
+		}
+	}
+	putchar('\n');
+}
