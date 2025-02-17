0f24dd0a879bbae87fae4af1737e7397c10ebb04
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 13:55:28 +0100

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char ch = 'abcdefghijklmnopqrstuvwxyz';
+
+	putchar(ch);
+
+	return (0);
+}
