3273cdab0e5679e603b31fe4026ffbbc5a6a272a
Osman Bagci <osman.bagci3@gmail.com>
2025-02-17 14:35:26 +0100

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+/*
+ * File: 4-print_alphabt.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ * 	except q and e
+ *
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (!ch == 'q' && !ch == 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
