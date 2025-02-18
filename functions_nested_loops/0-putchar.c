083180b161f4d116b48645d23563c8c1d7b2673f
Osman Bagci <osman.bagci3@gmail.com>
2025-02-18 11:34:33 +0100

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -23,7 +23,15 @@ int main(void)
 	char a = 'a';
 	char r = 'r';
 
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
 	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+
 
 	return (0);
 }
