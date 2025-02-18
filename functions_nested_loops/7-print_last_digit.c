252ec4087eca8c3de1978233f11c370efc70dbd9
Osman Bagci <osman.bagci3@gmail.com>
2025-02-18 14:59:10 +0100

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+/*
+ * File: 7-print_last_digit.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * print_last_digit - Get last digit of n
+ *
+ * @n : Parameter entre
+ *
+ * Return: 1 or 0.
+ */
+
+int print_last_digit (int n)
+{
+	int lastDigit = n % 10;
+
+	return (lastDigit);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -18,3 +18,5 @@ int _isalpha(char c);
 int print_sign(int n);
 
 int _abs(int n);
+
+int print_last_digit(int n);
