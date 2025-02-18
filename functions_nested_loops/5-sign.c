3d5284778ba91e6302040fe933340a592559471b
Osman Bagci <osman.bagci3@gmail.com>
2025-02-18 14:44:47 +0100

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+/*
+ * File: 5-sign.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * print_sign - Prints 0 or - or +
+ *
+ * @n : Parameter entree
+ *
+ * Return: 1 or 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	
+	return (0);
+}
