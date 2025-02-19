485daa114394a8fa8290e63476b4f6394dc89330
Osman Bagci <osman.bagci3@gmail.com>
2025-02-19 11:10:39 +0100

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -21,31 +21,30 @@ void times_table(void)
 	int lastDigit;
 	int firstDigit;
 
-	for(i = 0 ; i < 10 ; i++)
+	for( i = 0 ; i < 10 ; i++ )
 	{
-		for(j = 0 ; j < 10 ; j++)
+		for( j = 0 ; j < 10 ; j++ )
 		{
 			k = i * j;
-			if(k < 10)
+			if( k < 10 )
 			{
-				if(j > 0)
-                                {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
-                                }
+				if( j > 0 )
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				}
 				_putchar('0' + k);
 			}
 			else
 			{
 				lastDigit = k % 10;
 				firstDigit = k / 10;
-                                if(j > 0)
+				if(j > 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-                                }
-
+				}
 				_putchar('0' + firstDigit);
 				_putchar('0' + lastDigit);
 			}
