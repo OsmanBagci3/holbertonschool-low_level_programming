252f3d863119b07ff2a8707f97d617aeff669f6e
Osman Bagci <osman.bagci3@gmail.com>
2025-02-19 14:42:34 +0100

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -23,12 +23,12 @@ void print_to_98(int n)
 	{
 		for ( ; n < 98 ; n++)
 		{
-			if (! (n == 0))
+			if (!(n == 0))
 			{
 				centieme = _abs(n) / 100;
 				dizaine = (_abs(n) / 10) % 10;
 				unite = _abs(n) % 10;
-				if (n < 0) _putchar('-');
+				if (n < 0) {_putchar('-');}
 				if (!(centieme == 0)) _putchar('0' + centieme);
 				if (_abs(n) > 9) _putchar('0' + dizaine);
 				_putchar('0' + unite);
