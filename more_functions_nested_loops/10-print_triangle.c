8125577b8f131e7eb2bc9c6071cc41b29ab9348d
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 13:55:26 +0100

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+/*
+ * File: 10-print_triangle.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * print_triangle - Prints triangle
+ *
+ * @size: Parametre dentree
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = (size - i) - 1 ; j > 0  ; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
