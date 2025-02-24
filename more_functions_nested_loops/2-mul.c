ea585559dd3ef676a459c5168d64669c8c65df21
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 11:28:55 +0100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-mul.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * mul - Multiplies two integers
+ *
+ * @a : Parameter 1
+ * @b : Parameter 2
+ *
+ * Return: 1 or 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
