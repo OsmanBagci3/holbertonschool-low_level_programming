e48fe6071ed10e772d22858b2c665c6e9804223b
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 11:43:46 +0100

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+/*
+ * File: 5-more_numbers.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * more_numbers - Prints 10x numbers from 0 to 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; i < 15 ; i++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n')
+	}
+
+}
