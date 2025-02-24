1c18031de18a642b054856cac663e815250164b7
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 20:40:23 +0100

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-rev_string.c
+ */
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+#include "4-print_rev.c"
+/**
+ * rev_string - Print a string in reverse
+ *
+ * @s: Paramatre dentree
+ *
+ */
+
+void rev_string(char *s)
+{
+	print_rev(s);
+}
