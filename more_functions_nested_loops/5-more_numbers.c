9230059de7662eddc1f2f6f2ac80af0113a9af45
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 11:50:26 +0100

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,20 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int unite;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 15 ; j++)
 		{
-			_putchar(j + '0');
+			unite = j % 10;
+
+			if (j > 9)
+			{
+				_putchar('0' + j / 10);
+			}
+
+			_putchar('0' + unite);
 		}
 		_putchar('\n');
 	}
