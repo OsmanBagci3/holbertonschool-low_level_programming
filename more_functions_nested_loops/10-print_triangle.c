5da2252be91df760deccc6b0a40a3670b9c1aa49
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 14:04:23 +0100

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -26,12 +26,12 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
-			for (j = 0 ; j < i ; j++)
+			for (j = 0 ; j < i+1 ; j++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');	
 		}
-		_putchar('\n');
 	}
 	else
 	{
