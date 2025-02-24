ed930b5695ccb32d0f9a9f2ca3afe74f7cda925e
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 15:30:29 +0100

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-strleng.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * _strlen - Use of strlen
+ *
+ * @s: Paramatre dentree
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s))
+}
