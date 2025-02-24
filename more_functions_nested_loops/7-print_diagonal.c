cf7667773b8f867ffc370ee75ad05f1c0a012ae5
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 12:10:06 +0100

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -16,11 +16,16 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n > 0)
 	{
 		for (i = 0 ; i < n ; i++)
 		{
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
