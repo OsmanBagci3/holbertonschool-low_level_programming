7d45288c983ebf42e7813be90a61f522d8c6ce18
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 23:23:01 +0100

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+/*
+ * File: 6-puts2.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * puts2 - Use of strlen
+ *
+ * @str: Paramatre dentree
+ *
+ */
+
+void puts2(char *str)
+{
+}	
