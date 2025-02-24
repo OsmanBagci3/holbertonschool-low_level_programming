ac454a303cfe0cbeb970709619568d518e58e8eb
Osman Bagci <osman.bagci3@gmail.com>
2025-02-25 00:09:59 +0100

    8

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/*
+ * File: 9-strcpy.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * *_strcpy - Copy data from one string into the
+ *		the memory of the other string
+ *
+ * @dest: Paramatre dentree
+ * @src: Parametre dentree 2
+ *
+ * Return: Copied function
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
