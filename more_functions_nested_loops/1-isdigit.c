310484baed0d1c7303b98303b997eb5cb468426e
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 11:25:01 +0100

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+/*
+ * File: 1-isdigit.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * isdigit - check if c is a digit number
+ *
+ * @c : Parameter
+ *
+ * Return: 1 or 0.
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -8,3 +8,5 @@
 int _putchar(char c);
 
 int _isupper(char c);
+
+int _isdigit(int c);
