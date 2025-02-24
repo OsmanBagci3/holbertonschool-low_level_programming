3a0b08d059e20344472a017313ea576be2b444b1
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 19:38:44 +0100

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-puts.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * _strlen - Use of strlen
+ *
+ * @s: Paramatre dentree
+ *
+ * Return: size of table
+ */
+
+void _puts(char *str)
+{
+	
+	int i;
+
+	while(s[i] != '\0')
+	{
+		puts(s[i]);
+		i++;
+	}
+	puts("");
+}
