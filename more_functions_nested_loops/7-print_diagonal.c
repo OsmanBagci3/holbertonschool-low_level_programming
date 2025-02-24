53c5c490cdc66128e3f57f605772eafaa6fbb91e
Osman Bagci <osman.bagci3@gmail.com>
2025-02-24 12:07:19 +0100

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+/*
+ * File: 7-print_diagonal.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * print_diagonal - Print des diagonales
+ *
+ * @n: Parametre dentree
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+
