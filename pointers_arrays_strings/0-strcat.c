c00cae79f31fb1a54034366d81f7db78667bdcf4
Osman Bagci <osman.bagci3@gmail.com>
2025-02-25 16:59:35 +0100

    more pointer 0-

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -19,12 +19,19 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
+	int len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
 
 	while (src[i] != '\0')
 	{
-		dest += src[i];
+		dest[len] = src[i];
 		i++;
+		len++;
 	}
 
-	return (src);
+	return (dest);
 }		
