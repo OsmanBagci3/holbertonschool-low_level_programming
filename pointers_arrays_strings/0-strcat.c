e5052bd917d48015d65a0e296c5579ad93693f54
Osman Bagci <osman.bagci3@gmail.com>
2025-02-25 15:49:03 +0100

    more pointer 0-

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-str_cat.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * *_strcpy - Copy data from one string into the
+ *		the memory of the other string
+ *
+ * @dest: Paramatre dentree
+ * @src: Parametre dentree 2
+ *
+ * Return: Pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char *ptr = dest;
+
+	while (src[i] != '\0')
+	{
+		dest += src[i]
+	}
+
+	return (dest);
+}		
