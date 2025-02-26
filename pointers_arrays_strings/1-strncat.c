31c8157e13b7b01bea9012805bd35d0306b9577a
Osman Bagci <osman.bagci3@gmail.com>
2025-02-26 11:38:18 +0100

    Pointer 2 1-

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+/*
+ * File: 1-strn_cat.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * *_strncat - Concatenate data from one string into
+ *		the other string
+ *
+ * @dest: Paramatre dentree
+ * @src: Parametre dentree 2
+ *
+ * Return: Pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int len = 0;
+	int compteur = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	while (src[i] != '\0' || compteur > n)
+	{
+		dest[len] = src[i];
+		i++;
+		len++;
+		compteur++;
+	}
+
+	return (dest);
+}
