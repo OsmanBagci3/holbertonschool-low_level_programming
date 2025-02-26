8ad8783333d2b1c2403a67d4e0ff9facfb8f62be
Osman Bagci <osman.bagci3@gmail.com>
2025-02-26 18:21:07 +0100

    pointer 2 7-

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+/*
+ * File: 7-leet.c
+ */
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * *cap_string - Upper case characters
+ *
+ * @s: Paramatre dentree
+ *
+ * Return: pointer to s
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+		}
+	}
+
+	return (s);
+}
