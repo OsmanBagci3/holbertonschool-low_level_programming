1e7c9b6971e7b46c993c5cf021a73e6faf2e0f14
Osman Bagci <osman.bagci3@gmail.com>
2025-02-26 14:02:22 +0100

    pointer 2 4-

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+/*
+ * File: 4-rev_array.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * *_strncpy - Copy data from one string into the
+ *		the memory of the other string
+ *
+ * @dest: Paramatre dentree
+ * @src: Parametre dentree 2
+ * @n: Condition d arret
+ *
+ * Return: Pointer to dest
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tempValue;
+
+	for (i = 0 ; i < (n/2) ; i++)
+	{
+		tempValue = a[i];
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = tempValue;
+	}
+}
