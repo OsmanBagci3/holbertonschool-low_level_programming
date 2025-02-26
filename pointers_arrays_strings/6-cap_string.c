8a82fab2707307937e2daeca8f8d361351f69ea5
Osman Bagci <osman.bagci3@gmail.com>
2025-02-26 16:23:50 +0100

    pointeur 2 6-

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+/*
+ * File: 6-cap_string.c
+ */
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * *string_toupper - Upper case characters
+ *
+ * @s: Paramatre dentree
+ *
+ * Return: pointer to s
+ */
+
+char *cap_string(char *s)
+{
+	int i = 1;
+
+	while (s[i] != '\0')
+	{
+		if ((int)s[i] >= 97 && (int)s[i] <= 122)
+		{
+			if ((int)s[i-1] < 97 && (int)s[i-1] > 122 && (int)s[i-1] < 65 && (int)s[i-1] > 90)
+			{
+				s[i] = (int)s[i] -32;
+			}
+		}
+		i++;
+	}
+}
