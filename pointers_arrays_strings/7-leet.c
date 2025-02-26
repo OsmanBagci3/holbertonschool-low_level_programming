71b79fa84e4601d9aa088681520d8d83a65189d2
Osman Bagci <osman.bagci3@gmail.com>
2025-02-26 19:24:36 +0100

    pointer 2 7-

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -24,8 +24,18 @@ char *leet(char *s)
 			s[i] == 'E' || s[i] == 'o' || s[i] == 'O' || s[i] == 'l'
 			|| s[i] == 'L' || s[i] == 't' || s[i] == 'T')
 		{
+			s[i] = transfo(s[i]);
 		}
 		i++;
 	}
 	return (s);
+}
+
+char transfo(char t)
+{
+	if (t < 100)
+	{
+	}
+
+	return (t);
 }
