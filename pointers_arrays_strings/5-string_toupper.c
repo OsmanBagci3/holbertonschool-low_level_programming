4699810427abe093ae082afd75ded729b39e4896
Osman Bagci <osman.bagci3@gmail.com>
2025-02-26 15:49:57 +0100

    pointeur 2 5-

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,34 @@
+/*
+ * File: 5-string_toupper.c
+ */
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * reverse_array - Copy data from one string into the
+ *		the memory of the other string
+ *
+ * @a: Paramatre dentree
+ * @n: Condition d arret
+ *
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0 ; i < len ; i++)
+	{
+		toupper(s[i]);
+	}
+
+	return (s);
+}
