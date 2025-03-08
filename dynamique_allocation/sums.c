d8e972dae11ff64ce3fadce9ad2f7706b73b1880
Osman Bagci <osman.bagci3@gmail.com>
2025-03-08 22:45:33 +0100

    Somme des valeurs du tableau dynamique

---
+++ dynamique_allocation/sums.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main: Sum value of dynamic table
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int n;
+	int *p;
+	int a;
+	int i;
+	int sum = 0;
+
+	printf("Entrez la taille du tableau : ");
+	fflush( stdout );
+	scanf("%d", &n);
+
+	if (n <= 0)
+	{
+		printf("taille du tableau invalide : ");
+	}
+
+	p = (int *)malloc(n * sizeof(int));
+	
+	if (p == NULL)
+	{
+		printf("Echec de l'allocation mémoire\n");
+
+		return (1);
+	}
+
+	printf("Entrez %d valeurs : ", n);
+	fflush (stdout);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		scanf("%d", &a);
+		*(p + i) = a;
+	}
+	
+	printf("Somme des valeurs : ");
+
+	for (i = 0 ; i < n ; i++)
+	{
+		sum = sum + *(p + i);
+	}
+
+	free(p);
+
+	printf("%d", sum);
+	printf("\n");
+
+	p = NULL;
+
+	return (0);
+}
